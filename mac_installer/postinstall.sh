2271cc005a604915b297030a2ad205700ccdf7ef
David Ko <david.dk.ko@gmail.com>
2014-04-22 13:11:33 -0700

    Fixed postinstall script, added deploy instructions

--- mac_installer/postinstall.sh
+++ mac_installer/postinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd $2/QtBaroboLink.app/chbarobo/
+cd $2/BaroboLink.app
 sudo rm -rf /opt/ch/package/chbarobo
 sudo rm -f /opt/ch/toolkit/include/mobot.h
 sudo rm -f /opt/ch/toolkit/include/linkbot.h
@@ -9,4 +9,5 @@ sudo cp -R chbarobo /opt/ch/package/
 sudo mkdir -p /opt/ch/toolkit/include
 sudo cp /opt/ch/package/chbarobo/include/mobot.h /opt/ch/toolkit/include
 sudo cp /opt/ch/package/chbarobo/include/linkbot.h /opt/ch/toolkit/include
+touch /tmp/chbarobo_installed_2.0.7.txt
 exit 0
